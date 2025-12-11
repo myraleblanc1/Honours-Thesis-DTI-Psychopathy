7f2e2d70707c5106f065337b62928002d4bea95a
Myra LeBlanc <myra.leblanc@ontariotechu.net>
2025-12-11 13:39:37 -0500

    Add script to create ROI masks from JHU atlas
    This script creates region of interest (ROI) masks from the JHU atlas using fslmaths.

--- scripts/create_roi_masks.sh
+++ scripts/create_roi_masks.sh
@@ -1,2 +1,16 @@
-#create roi script
+ROI_DIR="/data/rois"
+mkdir -p $ROI_DIR
 
+ATLAS="$FSLDIR/data/atlases/JHU/JHU-ICBM-tracts-maxprob-thr0-1mm.nii.gz"
+
+# UF Left (label 21)
+fslmaths $ATLAS -thr 21 -uthr 21 -bin $ROI_DIR/UF_L.nii.gz
+
+# UF Right (label 22)
+fslmaths $ATLAS -thr 22 -uthr 22 -bin $ROI_DIR/UF_R.nii.gz
+
+# Dorsal Cingulum Left (CgC, label 31)
+fslmaths $ATLAS -thr 31 -uthr 31 -bin $ROI_DIR/DC_L.nii.gz
+
+# Dorsal Cingulum Right (CgC, label 32)
+fslmaths $ATLAS -thr 32 -uthr 32 -bin $ROI_DIR/DC_R.nii.gz
