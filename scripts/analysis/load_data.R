33d983b510e73c645882aa3f9505d32bb6d2a757
Myra LeBlanc <myra.leblanc@ontariotechu.net>
2025-12-13 11:24:26 -0500

    updated load_data script for prelim SSSP abstract analyses

--- scripts/analysis/load_data.R
+++ scripts/analysis/load_data.R
@@ -6,29 +6,29 @@ library(tidyverse)
 ################################################################################
 #MOVE ALL RAW DATA FILES INTO data/raw folder (you can drag and drop)
 #REPLACE FILENAME WITH THE ACTUAL FILENAME OF YOUR DATA
-raw_behaviouraldata <- read_csv("data/raw/FILENAME.csv")
-raw_imagingdata <- read_csv("data/raw/FILENAME.csv")
+raw_quesdata <- read_csv("data/raw/COrina_PCLr.csv")
+raw_imagingdata <- read_csv("data/raw/roi_metrics_ALL_SUBJECTS.csv")
 
-raw_Qualtricsdata <- read_csv("data/raw/FILENAME.csv")
+#CURRENT STUDY DOESN"T USE QUALTRICS MEASURES, SO THIS IS COMMENTED OUT
+#raw_Qualtricsdata <- read_csv("data/raw/FILENAME.csv")
 #Remove unecessary Qualtrics output
-raw_Qualtricsdata <- raw_Qualtricsdata[-c(2), ]
-colnames(raw_Qualtricsdata) <- as.character(raw_Qualtricsdata[1, ])
-raw_Qualtricsdata <- raw_Qualtricsdata[-1, ]
-raw_Qualtricsdata <- raw_Qualtricsdata <- raw_Qualtricsdata[, -(1:17)]
+#raw_Qualtricsdata <- raw_Qualtricsdata[-c(2), ]
+#colnames(raw_Qualtricsdata) <- as.character(raw_Qualtricsdata[1, ])
+#raw_Qualtricsdata <- raw_Qualtricsdata[-1, ]
+#raw_Qualtricsdata <- raw_Qualtricsdata <- raw_Qualtricsdata[, -(1:17)]
 
 #the dataframe names must stay the same for subsequent code to work properly 
 #(e.g. raw_behaviouraldata)
 ################################################################################
 
 #VIEW THE IMPORTED DATA
-View(raw_behaviouraldata)
+View(raw_quesdata)
 View(raw_imagingdata)
-View(raw_Qualtricsdata)
+#View(raw_Qualtricsdata)
 
-head(raw_behaviouraldata)
+head(raw_quesdata)
 head(raw_imagingdata)
-head(raw_Qualtricsdata)
-
+#head(raw_Qualtricsdata)
 #-------------------------------------------------------------------------------
 #MERGE DATASETS TOGETHER
 
@@ -37,8 +37,8 @@ head(raw_Qualtricsdata)
 #Change "COLUMN_NAME" to the actual column name in your datasets if it's different 
 #Leave this as a string (keep the "")
 #In QUALTRICS  this will likely output as Q(##) 
-raw_behaviouraldata <- raw_behaviouraldata |> rename(subject_id = "COLUMN_NAME")
-raw_imagingdata <- raw_imagingdata |> rename(subject_id = "COLUMN_NAME")
+raw_quesdata <- raw_quesdata |> rename(subject_id = "COLUMN_NAME")
+raw_imagingdata <- raw_imagingdata |> rename(subject_id = "subject")
 raw_Qualtricsdata <- raw_Qualtricsdata |> rename(subject_id = "COLUMN_NAME")
 
 #Ensure this is changed
@@ -46,12 +46,11 @@ View(raw_behaviouraldata)
 View(raw_imagingdata)
 View(raw_Qualtricsdata)
 
-rawdata <- raw_behaviouraldata %>%
-  left_join(raw_imagingdata,  by = "subject_id") %>%
-  left_join(raw_Qualtricsdata, by = "subject_id")
+rawdata <- raw_imagingdata %>%
+  left_join(raw_quesdata,  by = "subject_id") 
 
 #VIEW THE MERGED DATA
 View(rawdata)
 
 #SAVE THE MERGED DATA (as a .csv file)
-write_csv(rawdata, paste0("data/raw/rawdata.csv"))
+write_csv(rawdata, paste0("data/raw/rawdata_prelimSSSP_abstract.csv"))
