059fba852c590952bc8b9f6d45a96a87700b34f3
Myra LeBlanc <myra.leblanc@ontariotechu.net>
2025-12-15 14:03:19 -0500

    updated load data script for final SSSP abstract version

--- scripts/analysis/load_data.R
+++ scripts/analysis/load_data.R
@@ -7,9 +7,10 @@ library(tidyverse)
 #MOVE ALL RAW DATA FILES INTO data/raw folder (you can drag and drop)
 #REPLACE FILENAME WITH THE ACTUAL FILENAME OF YOUR DATA
 raw_quesdata <- read_csv("data/raw/COrina_PCLr.csv")
-raw_imagingdata <- read_csv("data/raw/roi_metrics_ALL_SUBJECTS.csv")
+raw_imagingdata <- read_csv("data/raw/roi_FA_ALL_SUBJECTS.csv")
+raw_IQdata <- read_csv("data/raw/IQ_data.csv")
 
-#CURRENT STUDY DOESN"T USE QUALTRICS MEASURES, SO THIS IS COMMENTED OUT
+#CURRENT STUDY DOESN'T USE QUALTRICS MEASURES, SO THIS IS COMMENTED OUT
 #raw_Qualtricsdata <- read_csv("data/raw/FILENAME.csv")
 #Remove unecessary Qualtrics output
 #raw_Qualtricsdata <- raw_Qualtricsdata[-c(2), ]
@@ -24,10 +25,12 @@ raw_imagingdata <- read_csv("data/raw/roi_metrics_ALL_SUBJECTS.csv")
 #VIEW THE IMPORTED DATA
 View(raw_quesdata)
 View(raw_imagingdata)
+View(raw_IQdata)
 #View(raw_Qualtricsdata)
 
 head(raw_quesdata)
 head(raw_imagingdata)
+head(rawIQdata)
 #head(raw_Qualtricsdata)
 #-------------------------------------------------------------------------------
 #MERGE DATASETS TOGETHER
@@ -37,9 +40,9 @@ head(raw_imagingdata)
 #Change "COLUMN_NAME" to the actual column name in your datasets if it's different 
 #Leave this as a string (keep the "")
 #In QUALTRICS  this will likely output as Q(##) 
-raw_quesdata <- raw_quesdata |> rename(subject_id = "COLUMN_NAME")
+#raw_quesdata <- raw_quesdata |> rename(subject_id = "COLUMN_NAME")
 raw_imagingdata <- raw_imagingdata |> rename(subject_id = "subject")
-raw_Qualtricsdata <- raw_Qualtricsdata |> rename(subject_id = "COLUMN_NAME")
+#raw_Qualtricsdata <- raw_Qualtricsdata |> rename(subject_id = "COLUMN_NAME")
 
 #Ensure this is changed
 View(raw_behaviouraldata)
@@ -47,7 +50,8 @@ View(raw_imagingdata)
 View(raw_Qualtricsdata)
 
 rawdata <- raw_imagingdata %>%
-  left_join(raw_quesdata,  by = "subject_id") 
+  left_join(raw_quesdata,  by = "subject_id") |>
+  left_join(raw_IQdata,    by = "subject_id")
 
 #VIEW THE MERGED DATA
 View(rawdata)
