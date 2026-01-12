43bb120e7a369617467eb176a85c9205dba76643
Myra LeBlanc <myra.leblanc@ontariotechu.net>
2026-01-12 18:58:50 -0500

    updated ROI mask creation script to reflect TBSS

--- scripts/create_roi_masks.sh
+++ scripts/create_roi_masks.sh
@@ -1,8 +1,13 @@
 ROI_DIR='/home/mleblanc/DTI_Psychopathy/Honours-Thesis-DTI-Psychopathy/data/rois'
+TBSS_STATS='/home/mleblanc/DTI_Psychopathy/Honours-Thesis-DTI-Psychopathy/data'
+
 mkdir -p $ROI_DIR
 
 ATLAS="$FSLDIR/data/atlases/JHU/JHU-ICBM-tracts-maxprob-thr0-1mm.nii.gz"
-
+SKELETON="$TBSS_STATS/mean_FA_skeleton.nii.gz"
+# ========================
+# 1. Creates volumetric ROIs
+# ========================
 # UF Left (label 21)
 fslmaths $ATLAS -thr 21 -uthr 21 -bin $ROI_DIR/UF_L.nii.gz
 
@@ -14,3 +19,37 @@ fslmaths $ATLAS -thr 31 -uthr 31 -bin $ROI_DIR/DC_L.nii.gz
 
 # Dorsal Cingulum Right (CgC, label 32)
 fslmaths $ATLAS -thr 32 -uthr 32 -bin $ROI_DIR/DC_R.nii.gz
+# ==================================
+# 2. Resamples ROIs to TBSS space
+# ==================================
+for roi in UF_L UF_R DC_L DC_R; do
+    flirt \
+      -in  $ROI_DIR/${roi}.nii.gz \
+      -ref $SKELETON \
+      -out $ROI_DIR/${roi}_resampled.nii.gz \
+      -applyxfm \
+      -interp nearestneighbour
+done
+
+# =========================
+# 3. Create group-level skeleton ROI masks
+# =========================
+
+for roi in UF_L UF_R DC_L DC_R; do
+    fslmaths \
+      $ROI_DIR/${roi}_resampled.nii.gz \
+      -mul $SKELETON \
+      $ROI_DIR/${roi}_skel.nii.gz
+done
+
+# =========================
+# 4. Sanity checks
+# =========================
+
+echo "Voxel counts in skeleton ROIs:"
+for roi in UF_L UF_R DC_L DC_R; do
+    echo -n "${roi}: "
+    fslstats $ROI_DIR/${roi}_skel.nii.gz -V
+done
+
+echo "Done. Skeleton ROI masks created."
