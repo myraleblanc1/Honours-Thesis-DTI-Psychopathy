bd2afe34c614200288b5ed050c53518b26424712
Myra LeBlanc <myra.leblanc@ontariotechu.net>
2026-01-14 13:31:47 -0500

    updated roi

--- scripts/resample_rois.sh
+++ scripts/resample_rois.sh
@@ -1,10 +1,10 @@
-#resample rois to work
+mkdir -p data/rois_mni152_2mm
 
-
-REF_FA="/home/mleblanc/DTI_Psychopathy/Honours-Thesis-DTI-Psychopathy/data/raw/M87114371/rdti_FA_FA_to_target.nii.gz"
-ROI_DIR="/home/mleblanc/DTI_Psychopathy/Honours-Thesis-DTI-Psychopathy/data/rois"
-
-flirt -in $ROI_DIR/UF_L.nii.gz -ref $REF_FA -applyxfm -usesqform -out $ROI_DIR/UF_L_resampled.nii.gz
-flirt -in $ROI_DIR/UF_R.nii.gz -ref $REF_FA -applyxfm -usesqform -out $ROI_DIR/UF_R_resampled.nii.gz
-flirt -in $ROI_DIR/DC_L.nii.gz -ref $REF_FA -applyxfm -usesqform -out $ROI_DIR/DC_L_resampled.nii.gz
-flirt -in $ROI_DIR/DC_R.nii.gz -ref $REF_FA -applyxfm -usesqform -out $ROI_DIR/DC_R_resampled.nii.gz
+for ROI in data/rois/*.nii.gz; do
+  flirt \
+    -in "$ROI" \
+    -ref $FSLDIR/data/standard/MNI152_T1_2mm.nii.gz \
+    -applyxfm \
+    -interp nearestneighbour \
+    -out data/rois_mni152_2mm/$(basename "$ROI")
+done
