a6489f69c4404ce0182ee93229331d5e4d366130
Myra LeBlanc <myra.leblanc@ontariotechu.net>
2026-01-14 23:57:59 -0500

    Create mask_fix.sh

---
+++ scripts/mask_fix.sh
@@ -0,0 +1,28 @@
+cd /home/mleblanc/DTI_Psychopathy/Honours-Thesis-DTI-Psychopathy
+
+mkdir -p TBSS_GROUP/rois_resamp
+mkdir -p TBSS_GROUP/rois_skel
+#==================================
+# Resample masks into TBSS space
+#==================================
+for roi in data/rois/*.nii.gz; do
+    name=$(basename "$roi" .nii.gz)
+
+    flirt \
+      -in "$roi" \
+      -ref TBSS_GROUP/stats/mean_FA_skeleton.nii.gz \
+      -applyxfm \
+      -usesqform \
+      -interp nearestneighbour \
+      -out TBSS_GROUP/rois_resamp/${name}_tbss.nii.gz
+done
+#==================================
+# Skeletonise the masks
+#==================================
+for roi in TBSS_GROUP/rois_resamp/*_tbss.nii.gz; do
+    name=$(basename "$roi" _tbss.nii.gz)
+
+    fslmaths "$roi" \
+      -mas TBSS_GROUP/stats/mean_FA_skeleton.nii.gz \
+      TBSS_GROUP/rois_skel/${name}_skel.nii.gz
+done
