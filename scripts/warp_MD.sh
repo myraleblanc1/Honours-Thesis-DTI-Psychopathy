00f087cb645eccc8285b271515c7ea15be7ab656
Myra LeBlanc <myra.leblanc@ontariotechu.net>
2026-01-15 12:32:11 -0500

    Fix applywarp command syntax in warp_MD.sh

--- scripts/warp_MD.sh
+++ scripts/warp_MD.sh
@@ -2,9 +2,9 @@ cd /home/mleblanc/DTI_Psychopathy/Honours-Thesis-DTI-Psychopathy
 SUBJ=M87100419
 cd data/raw/$SUBJ
 
-applywarp
---in=rdti_MD.nii.gz
---ref=target.nii.gz
---warp=rdti_FA_FA_to_target_warp.nii.gz
---out=rdti_MD_to_target.nii.gz
+applywarp \
+--in=rdti_MD.nii.gz \
+--ref=target.nii.gz \
+--warp=rdti_FA_FA_to_target_warp.nii.gz \
+--out=rdti_MD_to_target.nii.gz \
 --interp=spline
