d6600cb172be0587718f3b8d6fd0cfa42c6ca9f1
Myra LeBlanc <myra.leblanc@ontariotechu.net>
2026-01-21 13:27:08 -0500

    archived all the bash files for FA/AD/RD/MD and TBSS that were unused in the current pipeline

--- scripts/analysis/load_data.R
+++ scripts/analysis/load_data.R
@@ -7,7 +7,11 @@ library(tidyverse)
 #MOVE ALL RAW DATA FILES INTO data/raw folder (you can drag and drop)
 #REPLACE FILENAME WITH THE ACTUAL FILENAME OF YOUR DATA
 raw_quesdata <- read_csv("data/raw/COrina_PCLr.csv")
-raw_imagingdata <- read_csv("data/raw/roi_FA_ALL_SUBJECTS.csv")
+raw_imagingdata_FA <- read_csv("data/processed/roi_FA_values.csv")
+raw_imagingdata_MD <- read_csv("data/processed/roi_MD_values.csv")
+raw_imagingdata_AD <- read_csv("data/processed/roi_AD_values.csv")
+raw_imagingdata_RD <- read_csv("data/processed/roi_RD_values.csv")
+
 raw_IQdata <- read_csv("data/raw/IQ_data.csv")
 
 #CURRENT STUDY DOESN'T USE QUALTRICS MEASURES, SO THIS IS COMMENTED OUT
@@ -24,12 +28,18 @@ raw_IQdata <- read_csv("data/raw/IQ_data.csv")
 
 #VIEW THE IMPORTED DATA
 View(raw_quesdata)
-View(raw_imagingdata)
+View(raw_imagingdata_FA)
+View(raw_imagingdata_MD)
+View(raw_imagingdata_AD)
+View(raw_imagingdata_RD)
 View(raw_IQdata)
 #View(raw_Qualtricsdata)
 
 head(raw_quesdata)
-head(raw_imagingdata)
+head(raw_imagingdata_FA)
+head(raw_imagingdata_MD)
+head(raw_imagingdata_AD)
+head(raw_imagingdata_RD)
 head(rawIQdata)
 #head(raw_Qualtricsdata)
 #-------------------------------------------------------------------------------
@@ -41,7 +51,7 @@ head(rawIQdata)
 #Leave this as a string (keep the "")
 #In QUALTRICS  this will likely output as Q(##) 
 #raw_quesdata <- raw_quesdata |> rename(subject_id = "COLUMN_NAME")
-raw_imagingdata <- raw_imagingdata |> rename(subject_id = "subject")
+raw_imagingdata_FA <- raw_imagingdata_FA |> rename(subject_id = "subject")
 #raw_Qualtricsdata <- raw_Qualtricsdata |> rename(subject_id = "COLUMN_NAME")
 
 #Ensure this is changed
