cd25561d79adf355b01637cd993853f7aa74ad7e
Myra LeBlanc <myra.leblanc@ontariotechu.net>
2026-01-22 20:33:05 -0500

    Update create_roi_masks.sh

--- scripts/create_roi_masks.sh
+++ scripts/create_roi_masks.sh
@@ -15,3 +15,14 @@ fslmaths $ATLAS -thr 31 -uthr 31 -bin $ROI_DIR/DC_L.nii.gz
 
 # Dorsal Cingulum Right (CgC, label 32)
 fslmaths $ATLAS -thr 32 -uthr 32 -bin $ROI_DIR/DC_R.nii.gz
+
+#resample rois to work
+
+
+REF_FA="/home/mleblanc/DTI_Psychopathy/Honours-Thesis-DTI-Psychopathy/data/raw/M87114371/rdti_FA_FA_to_target.nii.gz"
+ROI_DIR="/home/mleblanc/DTI_Psychopathy/Honours-Thesis-DTI-Psychopathy/data/rois"
+
+flirt -in $ROI_DIR/UF_L.nii.gz -ref $REF_FA -applyxfm -usesqform -out $ROI_DIR/UF_L_resampled.nii.gz
+flirt -in $ROI_DIR/UF_R.nii.gz -ref $REF_FA -applyxfm -usesqform -out $ROI_DIR/UF_R_resampled.nii.gz
+flirt -in $ROI_DIR/DC_L.nii.gz -ref $REF_FA -applyxfm -usesqform -out $ROI_DIR/DC_L_resampled.nii.gz
+flirt -in $ROI_DIR/DC_R.nii.gz -ref $REF_FA -applyxfm -usesqform -out $ROI_DIR/DC_R_resampled.nii.gz
